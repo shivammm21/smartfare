cdfb9525d5bc438edf41c81300a994a1f8b132cb
Shivam Thorat <user@kali>
2024-11-15 10:01:34 +0530

    Initial commit

---
+++ src/main/java/com/fare/smart/SmartFareApplication.java
@@ -0,0 +1,14 @@
+package com.fare.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFareApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/fare/smart/SmartFareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fare.smart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
